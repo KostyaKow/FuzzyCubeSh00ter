efd3c308d42723f5f066063ea949e8d564be2068
Kostyantyn Kovalskyy <kostelkow@gmail.com>
2016-07-02 23:57:42 -0400

    Initial commit.

---
+++ Assets/EnemyMnMEminemAI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMnMEminemAI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+      name = "enemy";
+
+      gameObject.AddComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+
+
+TODO:
+   - [ ] good bullet path and crosshair
+   - [ ] multiplayer
+   - [ ] kake models for enemies
+   - [ ] make moving/shooting ai
+   - [ ] AK material/player model
+   - [ ] animate reloading
+   - [ ] check out rigging, normal mapping, textures
+   - [ ] re-learn blender
